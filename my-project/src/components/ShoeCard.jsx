5f1210b9901d9210480f0b4e7538fefde2077545
Parazyan_Roman <romaparazyan@gmail.com>
2023-08-20 13:38:29 +0300

    Nike project (full)

---
+++ my-project/src/components/ShoeCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ShoeCard = ({ imgUrl, changeBigShoe, bigShoeImg }) => {
+  const handeClick = () => {
+    if (bigShoeImg !== imgUrl.bigShoe) {
+      changeBigShoe(imgUrl.bigShoe)
+    }
+  }
+
+  return (
+    <div
+      className={`border-2 rounded-xl
+    ${
+      bigShoeImg === imgUrl.bigShoe ? 'border-coral-red' : 'border-transperent'
+    } cursor-pointer max-sm:flex-1
+    `}
+      onClick={handeClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img 
+        src={imgUrl.thumbnail}
+        alt="Shoe collection" 
+        width={127}
+        height={103}
+        className="object-contain"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ShoeCard
